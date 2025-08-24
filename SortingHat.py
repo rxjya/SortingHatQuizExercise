c774d0ce7d0f740bb1b437ba04adf35f4e0f67de
RojiyaRai <zmac399@live.rhul.ac.uk>
2025-08-24 18:17:51 +0100

    Added Q2 if statements

--- SortingHat.py
+++ SortingHat.py
@@ -14,3 +14,16 @@ elif answerq1 == 2:
   Slytherin= Slytherin+1
 else:
   print("Wrong input")
+
+# if statements for q2
+answerq2 = int(input(" Q2) When I'm dead, I want people to remember me as: 1) The Good 2) The Great 3) The Wise 4) The Bold - Enter your answer (1-4): "))
+if answerq2==1:
+  Hufflepuff= Hufflepuff+2
+elif answerq2==2:
+  Slytherin= Slytherin+2
+elif answerq2==3:
+  Ravenclaw= Ravenclaw+2
+elif answerq2==4:
+  Gryffindor= Gryffindor+2
+else:
+  print("Wrong input.")
